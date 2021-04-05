2b9b0abe8f6844550ddfbae28057febbeb6fb906
Juliomar de Matos Souza <infoexpress2014@gmail.com>
2021-04-04 22:55:27 -0300

    Terminado o jogo

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -7,11 +7,27 @@ class Pessoa:
         self.filhos = list(filhos)
 
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá, meu nome é  {self.nome}'
+
+    @staticmethod
+    def metodo_estatico():
+        return 42
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
+class Homem(Pessoa):
+    def cumprimentar(self):
+        cumprimentar_da_classe=super().cumprimentar()
+        return f'{cumprimentar_da_classe}. Aperto de mão'
+
+class Mutante(Pessoa):
+    olhos = 3
+
 
 if __name__ == '__main__':
-    renzo = Pessoa(nome='renzo')
-    luciano = Pessoa(renzo, nome ='Luciano')
+    renzo = Mutante(nome='renzo')
+    luciano = Homem(renzo, nome ='Luciano')
     print(Pessoa.cumprimentar(luciano))
     print(id(luciano))
     print(luciano.cumprimentar())
@@ -30,3 +46,11 @@ if __name__ == '__main__':
     print(luciano.olhos)
     print(renzo.olhos)
     print(id(Pessoa.olhos), id(luciano.olhos),id(renzo.olhos))
+    pessoa = Pessoa('Anonimo')
+    print(isinstance(pessoa, Pessoa))
+    print(isinstance(pessoa, Homem))
+    print(isinstance(renzo, Pessoa))
+    print(isinstance(renzo, Homem))
+    print(renzo.olhos)
+    print(luciano.cumprimentar())
+    print(renzo.cumprimentar())
